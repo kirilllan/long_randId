f6326c01687310845d31a1e5c123c4c1f8ad0bc3
kirilllan <77489120+kirilllan@users.noreply.github.com>
2021-11-12 14:07:16 +0200

    Update random.js

--- random.js
+++ random.js
@@ -1,12 +1,12 @@
 //** Get long  random id **/
 /** new Date().getTime() gives time in milliseconds since the start of time so in many cases its not suitable **/
-const longRandomId = iterations => {
-  let yourId = ""
+const longRandomID = iterations => {
+  let yourID = ""
   while (iterations > 0) {
     iterations--
-    yourId += Math.random().toString(36).replace(/^0\./, '')
+    yourID += Math.random().toString(36).replace(/^0\./, '')
   }
   //console.log(yourId)
-  return yourId
+  return yourID
 }
 longRandomId(213) //example output from argument 3: "b66fzmgzndooair51wxiwfuk3upbl0qe"
