cdbd69731369d44ed54ad4c0a08a7cafcb4d2142
kirilllan <s2100138@edu.bc.fi>
2021-11-12 13:15:41 +0200

    tidy JS version

--- random.js
+++ random.js
@@ -1,13 +1,12 @@
 //** Get long  random id **/
 /** new Date().getTime() gives time in milliseconds since the start of time so in many cases its not suitable **/
-/* JavaScript */
-const longRandomId = times => {
+const longRandomId = iterations => {
   let yourId = ""
-  while (times > 0) {
-    times--
-    yourId += Math.random().toString(36).toString().replace(/^0\./, '')
+  while (iterations > 0) {
+    iterations--
+    yourId += Math.random().toString(36).replace(/^0\./, '')
   }
   //console.log(yourId)
   return yourId
 }
-longRandomId(3) //example output from small number of 3: "b66fzmgzndooair51wxiwfuk3upbl0qe"
+longRandomId(213) //example output from argument 3: "b66fzmgzndooair51wxiwfuk3upbl0qe"
